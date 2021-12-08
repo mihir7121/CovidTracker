e3b2489e7e6c93a6065db152bc382bba8e767daa
Ameya Ranade <62432771+Ameya1104@users.noreply.github.com>
2021-12-08 22:30:14 +0530

    fetching data

--- app/views.py
+++ app/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import time
+import json
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
 def index(request):
     # context = {'latest_question_list': latest_question_list}
-    context = {}
+    attempt_num = 0
+    res = requests.get("http://localhost:3000/AN", timeout=10)
+    if res.status_code == 200:
+        data = json.loads(res.text)
+        data = json.dumps(data)
+        print(data)
+    else:
+        attempt_num += 1
+        time.sleep(5)
+    context = {data: data}
     return render(request, 'app/index.html', context)
